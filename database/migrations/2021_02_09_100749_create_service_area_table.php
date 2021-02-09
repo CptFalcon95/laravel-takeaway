12c4be85325b81907d08d91528f88e016502faa8
Alwindk <alwindekoop@gmail.com>
2021-02-09 11:20:18 +0100

    Database Migration

---
+++ database/migrations/2021_02_09_100749_create_service_area_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServiceAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_areas', function (Blueprint $table) {
+
+            $table->foreign('postcode_id')->references('id')->on('postcodes');
+            $table->foreign('business_id')->references('id')->on('business');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_areas');
+    }
+}
---
+++ database/migrations/2021_02_09_100911_create_business_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', 25);
+            $table->char('bio', 200);
+            $table->char('address', 25);
+            $table->char('est-delivery', 8);
+            $table->char('iban', 18);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business');
+    }
+}
