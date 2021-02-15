a58f9d8940165276ac66ef3de28f545f86c43561
Bernard Bondin <macbook@ber-macpro.lan>
2021-02-15 09:29:05 +0100

    added includes, layouts & pages

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('includes.header')
+</head>
+
+<body>
+
+    @yield ('content')
+
+    @include('includes.footer')
+    @include('includes.scripts')
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('team', function () {
+    return view('team');
+});
