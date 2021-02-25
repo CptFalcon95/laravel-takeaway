3e5757d0471d84b7612593095cbf75fc0cc4ea69
Falko <hfschoonewille@gmail.com>
2021-02-25 10:58:08 +0100

    working on the menu builder

--- resources/js/components/menuBuilder.js
+++ resources/js/components/menuBuilder.js
@@ -1,6 +1,7 @@
 $(function() {
-    var $formNewCat = $("#form-new-category");
-    var $menuRender = $(".menu-render-content");
+    let $formNewCat = $("#form-new-category");
+    let $menuRender = $(".menu-render-content");
+    let $categorySelect = $('select[name=categories]');
 
 
     $formNewCat.on('submit', function(e) {
@@ -10,13 +11,30 @@ $(function() {
             'name'  : $('input[name=name]').val(),
             'desc'  : $('textarea[name=desc]').val(),
             // 'image' : $('input[name=image]').val()
-            'image' : 'https://via.placeholder.com/1000x300'
+            'image' : 'https://via.placeholder.com/1000x200',
+            'csrf'  : $('input[name=_token]').val()
         };
 
         console.log(formData);
+        createCategoryElement(formData)
     });
 
     function createCategoryElement(data) {
+        let $wrapper = $('<div />').addClass('category-wrapper').attr('data-name', data.name);
+        let $card = $('<div />').addClass('card mb-3');
+        let $cardImg = $('<img />').addClass('card-image-top').attr('src', data.image);
+        let $cardBody = $('<div />').addClass('card-body');
+        let $cardTitle = $('<h5 />').addClass('card-title').html(data.name);
+        let $cardDesc = $('<p />').addClass('card-text').html(data.desc);
+
+        $cardImg.appendTo($card);
+        $cardBody.append($cardTitle).append($cardDesc).appendTo($card);
+        
+        $card.appendTo($wrapper);
+        $wrapper.appendTo($menuRender);
+    }
+
+    function createCategoryProduct(data) {
 
     }
 });
--- resources/views/business/menuBuilder/builder.blade.php
+++ resources/views/business/menuBuilder/builder.blade.php
@@ -10,9 +10,10 @@
 
     <div class="row">
         <div class="col-12">
-            <div class="menu-builder_add-category mt-5">
+            <div class="menu-builder_add-category">
                 <h3 class="menu-builder_title">New category</h3>
                 <form id="form-new-category" class="menu-builder_add-category-form mt-3">
+                    @csrf
                     <div class="form-group">
                         <label for="">{{__('Category name')}}</label>
                         <input class="form-control" type="text" name="name">
@@ -29,7 +30,42 @@
                 </form>
             </div>
         </div>
+    </div>
 
+    <div class="border-top my-3"></div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="menu-builder_add-product">
+                <h3 class="menu-builder_title">New product</h3>
+                <form id="form-new-product" class="menu-builder_add-product-form mt-2">
+                    @csrf
+                    <div class="form-group">
+                        <label for="">{{__('Product name')}}</label>
+                        <input class="form-control" type="text" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">{{__('Image url')}}</label>
+                        <input class="form-control" type="text" name="image">
+                    </div>
+                    <div class="form-group">
+                        <label for="">{{__('Product description')}}</label>
+                        <textarea class="form-control w-100" name="desc" id="new-product-desc"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="">{{__('Product price')}}</label>
+                        <input class="form-control" type="text" name="image">
+                    </div>
+                    <div class="form-group">
+                        <label for="">{{__('Product price')}}</label>
+                        <select name="category" id="categorySelect">
+                            
+                        </select>
+                    </div>
+                    <button class="btn btn-primary w-100" type="submit">{{__('Add')}} +</button>
+                </form>
+            </div>
+        </div>
     </div>
 
 </div>
